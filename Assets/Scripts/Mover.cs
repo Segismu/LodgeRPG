f1184b3f0cc8515fa04cc38d35f195a17a574ff5
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-08-17 23:39:36 -0300

    player movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,6 +10,22 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
     }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
 }
