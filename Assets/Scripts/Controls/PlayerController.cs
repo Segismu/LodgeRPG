cbc892e6c3ef3ae17fd93c2d42048be6ec55998c
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-09-17 23:40:10 -0300

    NavMesh path limit.

--- Assets/Scripts/Controls/PlayerController.cs
+++ Assets/Scripts/Controls/PlayerController.cs
@@ -26,6 +26,7 @@ namespace RPG.Controls
 
         [SerializeField] CursorMapping[] cursorMappings = null;
         [SerializeField] float maxNavMeshProjectionDistance = 1f;
+        [SerializeField] float maxNavPathLength = 30f;
 
         private void Awake()
         {
@@ -119,7 +120,26 @@ namespace RPG.Controls
             if (!hasCastToNavMesh) return false;
 
             target = navMeshHit.position;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            if (!hasPath) return false;
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLength(path) > maxNavPathLength) return false;
+
             return true;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float total = 0;
+            if (path.corners.Length < 2) return total;
+            for (int i = 0; i < path.corners.Length -1; i++)
+            {
+                total += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+
+            return total;
         }
 
         private void SetCursor(CursorType type)
