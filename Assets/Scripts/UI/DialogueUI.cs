e55eb4432269c1792b4b868475b01cb29dbfba71
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-10-14 11:52:35 -0300

    First dialogue implementation.

---
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Dialogue;
+using TMPro;
+
+namespace RPG.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        PlayerConversant playerConversant;
+        [SerializeField] TextMeshProUGUI AIText;
+
+        void Start()
+        {
+            playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            AIText.text = playerConversant.GetText();
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
