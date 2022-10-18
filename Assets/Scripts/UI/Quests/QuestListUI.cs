1d1bbcad7902f4aca5a683ad09aba68481f12291
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-10-18 17:17:10 -0300

    More GUI logic (quest tooltips).

--- Assets/Scripts/UI/Quests/QuestListUI.cs
+++ Assets/Scripts/UI/Quests/QuestListUI.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class QuestListUI : MonoBehaviour
 {
-    [SerializeField] Quest[] tempQuests;
     [SerializeField] QuestItemUI questPrefab;
 
     void Start()
@@ -17,11 +16,11 @@ public class QuestListUI : MonoBehaviour
 
         transform.DetachChildren();
 
-
-        foreach (Quest quest in tempQuests)
+        QuestList questList = GameObject.FindGameObjectWithTag("Player").GetComponent<QuestList>();
+        foreach (QuestStatus status in questList.GetStatuses())
         {
             QuestItemUI uiInstance = Instantiate<QuestItemUI>(questPrefab, transform);
-            uiInstance.Setup(quest);
+            uiInstance.Setup(status);
         }
     }
 }
---
+++ Assets/Scripts/UI/Quests/QuestTooltipUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Quests;
+using TMPro;
+using UnityEngine;
+
+namespace RPG.UI.Quests
+{
+    public class QuestTooltipUI : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI title;
+        [SerializeField] Transform objectiveContainer;
+        [SerializeField] GameObject objectivePrefab;
+        [SerializeField] GameObject objectiveIncompletePrefab;
+
+        public void Setup(QuestStatus status)
+        {
+            Quest quest = status.GetQuest();
+            title.text = quest.GetTitle();
+            objectiveContainer.DetachChildren();
+
+            foreach (string objective in quest.GetObjectives())
+            {
+                GameObject prefab = objectiveIncompletePrefab;
+
+                if (status.IsObjectiveComplete(objective))
+                {
+                    prefab = objectivePrefab;
+                }
+
+                GameObject objectiveInstance = Instantiate(prefab, objectiveContainer);
+                TextMeshProUGUI objectiveText = objectiveInstance.GetComponentInChildren<TextMeshProUGUI>();
+                objectiveText.text = objective;
+            }
+        }
+    }
+}
