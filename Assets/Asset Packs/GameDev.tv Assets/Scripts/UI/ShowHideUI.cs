c34f73c87ea2b6f2fbc8d9818b1c3f50af80ee9a
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-10-18 13:34:22 -0300

    More quest UI.

--- Assets/Asset Packs/GameDev.tv Assets/Scripts/UI/ShowHideUI.cs
+++ Assets/Asset Packs/GameDev.tv Assets/Scripts/UI/ShowHideUI.cs
@@ -9,19 +9,22 @@ namespace GameDevTV.UI
         [SerializeField] KeyCode toggleKey = KeyCode.Escape;
         [SerializeField] GameObject uiContainer = null;
 
-        // Start is called before the first frame update
         void Start()
         {
             uiContainer.SetActive(false);
         }
 
-        // Update is called once per frame
         void Update()
         {
             if (Input.GetKeyDown(toggleKey))
             {
-                uiContainer.SetActive(!uiContainer.activeSelf);
+                Toggle();
             }
+        }
+
+        public void Toggle()
+        {
+            uiContainer.SetActive(!uiContainer.activeSelf);
         }
     }
 }
