1922385891f58cf21934b31e48a585a39e51bdfc
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-10-19 17:42:33 -0300

    Other changes.

--- Assets/Scripts/Core/Condition.cs
+++ Assets/Scripts/Core/Condition.cs
@@ -7,23 +7,62 @@ namespace RPG.Core
     [System.Serializable]
     public class Condition
     {
-        [SerializeField] string predicate;
-        [SerializeField] string[] parameters;
+        [SerializeField]
+        Disjunction[] and;
 
         public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
         {
-            foreach (var evaluator in evaluators)
+            foreach (Disjunction dis in and)
             {
-                bool? result = evaluator.Evaluate(predicate, parameters);
-
-                if (result == null)
+                if (!dis.Check(evaluators))
                 {
-                    continue;
+                    return false;
                 }
-
-                if (result == false) return false;
             }
             return true;
         }
-    }
+
+        [System.Serializable]
+        class Disjunction
+        {
+            [SerializeField]
+            Predicate[] or;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach (Predicate pred in or)
+                {
+                    if (pred.Check(evaluators))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        [System.Serializable]
+        class Predicate
+    {
+            [SerializeField] string predicate;
+            [SerializeField] string[] parameters;
+            [SerializeField] bool negate = false;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach (var evaluator in evaluators)
+                {
+                    bool? result = evaluator.Evaluate(predicate, parameters);
+
+                    if (result == null)
+                    {
+                        continue;
+                    }
+
+                    if (result == negate) return false;
+                }
+                return true;
+            }
+        }
+    }  
 }
