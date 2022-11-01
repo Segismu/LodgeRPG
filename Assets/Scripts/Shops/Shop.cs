9439eb1d3c9a4d3324e2d6a9db7020a71faefbb9
PabloCS <titanbenevolo@icloud.com>
2022-11-01 12:13:08 -0300

    Integrating shop item rows.

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -11,17 +11,16 @@ namespace RPG.Shops
     {
         [SerializeField] string shopName;
 
-        public class ShopItem
-        {
-            InventoryItem item;
-            int availability;
-            float price;
-            int quantityInTransaction;
-        }
-
         public event Action onChange;
 
-        public IEnumerable<ShopItem> GetFilteredItems() { return null; }
+        public IEnumerable<ShopItem> GetFilteredItems()
+        {
+            yield return new ShopItem(InventoryItem.GetFromID("86cdcb8c-72f4-460b-88e4-450a28caf014"), 5, 10f, 0);
+            yield return new ShopItem(InventoryItem.GetFromID("61726d9e-d1d6-4c83-b2d9-2885452a6221"), 1, 100f, 0);
+            yield return new ShopItem(InventoryItem.GetFromID("88233aff-9d10-45c0-b905-98d0397b3bf9"), 10, 50f, 0);
+            yield return new ShopItem(InventoryItem.GetFromID("88233aff-9d10-45c0-b905-98d0397b3bf9"), 10, 50f, 0);
+
+        }
         public void SelectFilter(ItemCategory category) { }
         public ItemCategory GetFilter() { return ItemCategory.None; }
         public void SelectMode(bool isBuying) { }
