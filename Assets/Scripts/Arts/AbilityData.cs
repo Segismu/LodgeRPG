5e3ea05f0d796772aee52f097cf6213eeb41664f
PabloCS <titanbenevolo@icloud.com>
2022-11-11 16:12:48 -0300

    Added aoe prefaf effect.
    Working. Check bug when damage of efect on dead targets is infinite an results in infinite xp and loot.

--- Assets/Scripts/Arts/AbilityData.cs
+++ Assets/Scripts/Arts/AbilityData.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AbilityData
 {
     GameObject user;
+    Vector3 targetedPoint;
     IEnumerable<GameObject> targets;
 
     public AbilityData(GameObject user)
@@ -25,5 +27,20 @@ public class AbilityData
     public void SetTargets(IEnumerable<GameObject> targets)
     {
         this.targets = targets;
+    }
+
+    public Vector3 GetTargetedPoint()
+    {
+        return targetedPoint;
+    }
+
+    public void SetTargetedPoint(Vector3 targetedPoint)
+    {
+        this.targetedPoint = targetedPoint;
+    }
+
+    public void StartCoroutine(IEnumerator coroutine)
+    {
+        user.GetComponent<MonoBehaviour>().StartCoroutine(coroutine);
     }
 }
---
+++ Assets/Scripts/Arts/Effects/SpawnTargetPrefabEffect.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Arts.Effects
+{
+    [CreateAssetMenu(fileName = "Spawn Target Prefab Effect", menuName = "Arts/Effects/Effect Prefab")]
+    public class SpawnTargetPrefabEffect : EffectStrategy
+    {
+        [SerializeField] Transform prefabToSpawn;
+        [SerializeField] float destroyDelay = -1;
+
+        public override void StartEffect(AbilityData data, Action finished)
+        {
+            data.StartCoroutine(Effect(data, finished));
+        }
+
+        private IEnumerator Effect(AbilityData data, Action finished)
+        {
+            Transform instance = Instantiate(prefabToSpawn);
+            instance.position = data.GetTargetedPoint();
+
+            if (destroyDelay > 0)
+            {
+                yield return new WaitForSeconds(destroyDelay);
+                Destroy(instance.gameObject);
+            }
+            finished();
+        }
+    }
+}
