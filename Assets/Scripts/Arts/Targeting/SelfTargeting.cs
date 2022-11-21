fe05f7035dab916c90cf3b878656e870c252c558
PabloCS <titanbenevolo@icloud.com>
2022-11-21 11:58:54 -0300

    Added healing effects.

---
+++ Assets/Scripts/Arts/Targeting/SelfTargeting.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace RPG.Arts.Targeting
+{
+    [CreateAssetMenu(fileName = "Self Targeting", menuName = "Arts/Targeting/Self", order = 0)]
+    public class SelfTargeting : TargetingStrategy
+    {
+        public override void StartTargeting(AbilityData data, Action finished)
+        {
+            data.SetTargets(new GameObject[]{data.GetUser()});
+            data.SetTargetedPoint(data.GetUser().transform.position);
+            finished();
+        }
+    }
+}
